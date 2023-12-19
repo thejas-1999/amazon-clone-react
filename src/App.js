2875362cd5ccdc3a32b0e8b8dfb14c1f8a731afc
thejas-1999 <thejask074@gmail.com>
2023-12-19 07:11:22 +0530

    There is a little bit confusion in react router so temperarly stop this project and later start this project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
 
 function App() {
   return (
     //BEM
-    <div className="App">
-      {/* Header */}
-      <Header />
-      {/* Home */}
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/">
+          {/* Header */}
+          <Header />
+          {/* Home */}
+          <Home />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
